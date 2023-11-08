06dadeba010c95ab71f82d6b5ea4f74bbea6962e
JWrightDev <jimmywright77@outlook.com>
2023-11-08 14:17:51 -0500

    Changed links to open in new window, added a new property

--- src/app/helperFiles/ProjectDb.ts
+++ src/app/helperFiles/ProjectDb.ts
@@ -1,22 +1,33 @@
-import {Project} from "./Project";
+import { Project } from './Project';
 
 export const PROJECTLIST: Project[] = [
-
-  {
-    id: 2,
-    projectName: '1191 Walker Road. McGregor, ON.',
-    projectDescription: 'Coming Soon!',
-    projectCategory: 'Office Space For Lease',
-    thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
-    projectContactLink: ''
-  },
-  {
-    id: 3,
-    projectName: 'Miss MJ Ship',
-    projectDescription: 'Coming Soon!',
-    projectCategory: 'Future Project',
-    thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
-    projectContactLink: '',
-    isNew: true,
-  },
-]
+	{
+		id: 2,
+		projectName: '1191 Walker Road. McGregor, ON.',
+		projectDescription: 'Coming Soon!',
+		projectCategory: 'Office Space For Lease',
+		thumbnailLink:
+			'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
+		projectContactLink: '',
+	},
+	{
+		id: 3,
+		projectName: 'Miss MJ Ship, Amherstburg, ON.',
+		projectDescription: 'Coming Soon!',
+		projectCategory: 'Future Project',
+		thumbnailLink:
+			'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
+		projectContactLink: '',
+		isNew: true,
+	},
+	{
+		id: 4,
+		projectName: 'Queen St. Apartments, Harrow, ON.',
+		projectDescription: 'Coming Soon!',
+		projectCategory: 'Future Project',
+		thumbnailLink:
+			'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
+		projectContactLink: '',
+		isNew: true,
+	},
+];
